4f58444406e57ed46293d432b07252116e9b458a
Felipe Gaete Troncoso <f.gaete@duocuc.cl>
2025-06-28 22:58:44 -0400

    pruebas unitarias faltantes

--- src/app/pages/reporte-incidentes/reporte-incidentes.page.spec.ts
+++ src/app/pages/reporte-incidentes/reporte-incidentes.page.spec.ts
@@ -1,17 +1,48 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReporteIncidentesPage } from './reporte-incidentes.page';
+import { ReportesService } from 'src/app/services/reportes/reportes.service';
+import { of, throwError } from 'rxjs';
 
 describe('ReporteIncidentesPage', () => {
   let component: ReporteIncidentesPage;
   let fixture: ComponentFixture<ReporteIncidentesPage>;
+  let reportesServiceSpy: jasmine.SpyObj<ReportesService>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    reportesServiceSpy = jasmine.createSpyObj('ReportesService', ['obtenerReportes']);
+
+    await TestBed.configureTestingModule({
+      declarations: [ReporteIncidentesPage],
+      providers: [
+        { provide: ReportesService, useValue: reportesServiceSpy }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ReporteIncidentesPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('debería crear el componente', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('debería cargar y ordenar los reportes por fecha descendente', () => {
+    const mockReportes = [
+      { usuario: 'a', estacion: 'X', descripcion: 'desc1', fecha: '2025-06-10T10:00:00Z' },
+      { usuario: 'b', estacion: 'Y', descripcion: 'desc2', fecha: '2025-06-12T10:00:00Z' }
+    ];
+    reportesServiceSpy.obtenerReportes.and.returnValue(of(mockReportes));
+    component.ngOnInit();
+    expect(component.reportes[0].fecha).toBe('2025-06-12T10:00:00Z');
+    expect(component.reportes[1].fecha).toBe('2025-06-10T10:00:00Z');
+    expect(component.cargando).toBeFalse();
+  });
+
+  it('debería manejar error al cargar reportes', () => {
+    reportesServiceSpy.obtenerReportes.and.returnValue(throwError(() => new Error('error')));
+    component.ngOnInit();
+    expect(component.reportes).toEqual([]);
+    expect(component.cargando).toBeFalse();
   });
 });
